33093bc61c782f30351e44c78f8d9f4e8b13c6d6
Adrian <58171526+Adriansng@users.noreply.github.com>
2020-01-15 21:57:18 +0100

    -Layout DetailActivity NO ---> Drawable Phone/Position/Facebook -Launch DetailActivity CHECK -DetailActivity: CHECK --Button CHECK --Add favorite CHECK --URL avatar CHECK -Test UI Check -Test unite NO -Button Add random NO

---
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/utils/EspressoTestsMatchers.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.entrevoisins.utils;
+
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+public class EspressoTestsMatchers {
+
+    public static Matcher<View> withDrawable (final int resourceId) {
+        return new DrawableMatcher (resourceId);
+    }
+
+}
