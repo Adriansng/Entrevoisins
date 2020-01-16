ee6c8091058e4317f1649bfd56f096d7a61e2509
Adrian <58171526+Adriansng@users.noreply.github.com>
2020-01-16 01:35:26 +0100

    -Layout DetailActivity NO ---> Drawable Phone/Position/Facebook -Launch DetailActivity CHECK -DetailActivity: CHECK --Button CHECK --Add favorite CHECK --URL avatar CHECK -Test UI Check -Test unite Check -Button Add random NO

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.entrevoisins.service;
 
+import com.bumptech.glide.Glide;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.ui.neighbour_list.DetailNeighbourActivity;
 
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Before;
@@ -11,8 +13,11 @@ import org.junit.runners.JUnit4;
 
 import java.util.List;
 
+import static java.lang.System.load;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -21,6 +26,7 @@ import static org.junit.Assert.assertThat;
 public class NeighbourServiceTest {
 
     private NeighbourApiService service;
+    private DetailNeighbourActivity detailActivity;
 
     @Before
     public void setup() {
@@ -43,9 +49,20 @@ public class NeighbourServiceTest {
 
     @Test
     public void addNeighbourFavorite() {
-        List<Neighbour> favoriteNeighbours = service.getFavoriteNeighbours();
-        List<Neighbour> expectedFavoriteNeighbours = DummyNeighbourGenerator.DUMMY_NEIGHBOURS;
+        Neighbour neighbour= service.getNeighbours().get(0);
+        assertEquals(3,service.getFavoriteNeighbours().size());
+        assertFalse(neighbour.getFavorite());
+        neighbour.setFavorite(true);
+        assertEquals(4,service.getFavoriteNeighbours().size());
+        assertTrue(neighbour.getFavorite());
+    }
 
-
+    @Test
+    public void DetailAvatarNeighbour() {
+        Neighbour neighbour= service.getNeighbours().get(0);
+        neighbour.setAvatarUrl("Url");
+        neighbour.setName("Name");
+        assertEquals("Url",neighbour.getAvatarUrl());
+        assertEquals("Name",neighbour.getName());
     }
 }
