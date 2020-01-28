f20f0d150095534dbf1df632763e4f3ba6f3e69d
Adrian <58171526+Adriansng@users.noreply.github.com>
2020-01-28 17:32:08 +0100

    -Layout DetailActivity CHECK -Launch DetailActivity CHECK -DetailActivity: CHECK --Button CHECK --Add favorite CHECK --URL avatar CHECK -Test UI CHECK -Test unite CHECK -Button Add random CHECK

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -18,7 +18,7 @@ import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 import java.util.Objects;
 
 
-public class DetailNeighbourActivity extends AppCompatActivity{
+public class DetailNeighbourActivity extends AppCompatActivity {
     private NeighbourApiService mApiService;
     private boolean favoriteNeighbour;
 
@@ -26,35 +26,29 @@ public class DetailNeighbourActivity extends AppCompatActivity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_neighbour);
-        setSupportActionBar( findViewById(R.id.toolbar2));
+        setSupportActionBar(findViewById(R.id.toolbar2));
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        mApiService= DI.getNeighbourApiService();
+        mApiService = DI.getNeighbourApiService();
 
         /* ID */
-
         int idNeighbour = getIntent().getIntExtra("ID_NEIGHBOUR", 1);
 
         /* NAME NEIGHBOUR */
-
         String nameAvatar = getIntent().getStringExtra("NAME_NEIGHBOUR");
         CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingTollBar);
         collapsingToolbarLayout.setTitle(nameAvatar);
 
         /* NAME DESCRIPTION */
-
-        TextView nameDescription =  findViewById(R.id.name_description_txt);
+        TextView nameDescription = findViewById(R.id.name_description_txt);
         nameDescription.setText(nameAvatar);
 
         /* URL AVATAR */
-
         String neighbourAvatar = getIntent().getStringExtra("AVATAR_NEIGHBOUR");
-        ImageView avatarNeighbour =  findViewById(R.id.detail_avatar_neighbour);
-
+        ImageView avatarNeighbour = findViewById(R.id.detail_avatar_neighbour);
         RequestOptions options = new RequestOptions()
                 .centerCrop()
                 .placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_foreground);
-
         Glide.with(this).load(neighbourAvatar).apply(options).into(avatarNeighbour);
 
 
@@ -63,25 +57,25 @@ public class DetailNeighbourActivity extends AppCompatActivity{
         favoriteNeighbour = mApiService.getNeighbours(idNeighbour).getFavorite();
         ImageButton buttonFavor = findViewById(R.id.favory_button);
 
-        if(favoriteNeighbour){
+        if (favoriteNeighbour) {
             buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.yellow_star));
-            }else {
+        } else {
             buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.no_yellow_star));
-             }
+        }
 
         buttonFavor.setOnClickListener(v -> {
 
-                if (!favoriteNeighbour) {
-                    buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.yellow_star));
-                    mApiService.getNeighbours(idNeighbour).setFavorite(true);
-                    favoriteNeighbour =true;
+            if (!favoriteNeighbour) {
+                buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.yellow_star));
+                mApiService.getNeighbours(idNeighbour).setFavorite(true);
+                favoriteNeighbour = true;
 
 
-                } else {
-                    buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.no_yellow_star));
-                    mApiService.getNeighbours(idNeighbour).setFavorite(false);
-                    favoriteNeighbour=false;
-                }
+            } else {
+                buttonFavor.setImageDrawable(getResources().getDrawable(R.drawable.no_yellow_star));
+                mApiService.getNeighbours(idNeighbour).setFavorite(false);
+                favoriteNeighbour = false;
+            }
 
         });
 
